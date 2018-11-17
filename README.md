2d42713d036a2cdf6b1f91582fa3c27db3c76dc9
Vijay Deepak <vijaydeepak7195@outlook.com>
2018-11-17 16:59:14 +0530

    inital types and compiler

---
+++ README.md
@@ -0,0 +1,9 @@
+# Understanding TS
+
+    Understanding the basic and core consepts of typescript
+
+### Command References:
+
+    1. tsc --init --> to generate tsconfig.json with basic configurations.
+
+    2. tsc --> to compaile all ts files.
---
+++ exersise1.ts
@@ -0,0 +1,26 @@
+// type BankAccount = {
+//   money: number;
+//   deposite: (val: number) => void;
+// };
+
+// let bankAccount: BankAccount = {
+//   money: 2000,
+//   deposite(value: number) {
+//     this.money += value;
+//   }
+// };
+
+// type Customer = {
+//   name: string;
+//   bankAccount: BankAccount;
+//   hobbies: string[];
+// };
+
+// let mySelf: Customer = {
+//   name: "Vj",
+//   bankAccount: bankAccount,
+//   hobbies: ["Chess", "coding"]
+// };
+
+// mySelf.bankAccount.deposite(3000);
+// console.log(mySelf);
